ff017ebcf3517e153381980b90ec1ea57ca655f2
lalaibrahimzade <lalaibrahimzade19@gmail.com>
2023-11-28 14:43:49 +0300

    commit changed

--- components/settings/changePassword.vue
+++ components/settings/changePassword.vue
@@ -1,59 +1,75 @@
 <template>
-  <div class="change-password">
-    <div class="pass-input">
-      <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
-      <input
-        :type="oldPasswordVisible ? 'text' : 'password'"
-        class="password"
-        placeholder="Old password"
-      />
-      <div @click="oldPasswordVisible = !oldPasswordVisible">
-        <i
-          class="eye-icon"
-          :class="oldPasswordVisible ? 'fas fa-eye-slash ' : 'fas fa-eye'"
-        ></i>
+  <div>
+    <SettingModal v-if="settingModalIsOpen" @close="settingModalIsOpen=false"/>
+    <div class="change-password">
+      <div class="pass-input">
+        <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
+        <input
+          :type="oldPasswordVisible ? 'text' : 'password'"
+          class="password"
+          placeholder="Old password"
+        />
+        <div @click="oldPasswordVisible = !oldPasswordVisible">
+          <i
+            class="eye-icon"
+            :class="oldPasswordVisible ? 'fas fa-eye-slash ' : 'fas fa-eye'"
+          ></i>
+        </div>
       </div>
-    </div>
-    <div class="pass-input">
-      <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
-      <input
-        :type="newPasswordVisible ? 'text' : 'password'"
-        class="password"
-        placeholder="New password"
-      />
-      <div @click="newPasswordVisible = !newPasswordVisible">
-        <i
-          class="eye-icon"
-          :class="newPasswordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'"
-        ></i>
+      <div class="pass-input">
+        <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
+        <input
+          :type="newPasswordVisible ? 'text' : 'password'"
+          class="password"
+          placeholder="New password"
+        />
+        <div @click="newPasswordVisible = !newPasswordVisible">
+          <i
+            class="eye-icon"
+            :class="newPasswordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'"
+          ></i>
+        </div>
       </div>
-    </div>
-    <div class="pass-input">
-      <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
-      <input
-        :type="repeatNewPasswordVisible ? 'text' : 'password'"
-        class="password"
-        placeholder="Repeat password"
-      />
-      <div @click="repeatNewPasswordVisible = !repeatNewPasswordVisible">
-        <i
-          class="eye-icon"
-          :class="repeatNewPasswordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'"
-        ></i>
+      <div class="pass-input">
+        <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
+        <input
+          :type="repeatNewPasswordVisible ? 'text' : 'password'"
+          class="password"
+          placeholder="Repeat password"
+        />
+        <div @click="repeatNewPasswordVisible = !repeatNewPasswordVisible">
+          <i
+            class="eye-icon"
+            :class="
+              repeatNewPasswordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'
+            "
+          ></i>
+        </div>
       </div>
+      <button class="save-btn" @click="settingModalHandle(true)">Save</button>
     </div>
   </div>
 </template>
 
 <script>
+import SettingModal from "./settingModal";
+
 export default {
   name: "changePassword",
+  components: { SettingModal },
   data() {
     return {
+      settingModalIsOpen: false,
       oldPasswordVisible: false,
       newPasswordVisible: false,
       repeatNewPasswordVisible: false,
     };
+  },
+  methods: {
+    settingModalHandle(param){
+        this.settingModalIsOpen=true
+        this.$emit('settingModalOpenned')
+    },
   },
 };
 </script>
---
+++ components/settings/settingModal.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="setting-modal">
+    <div class="setting-modal-header">
+        <div class="close" @click="$emit('close')">x</div>
+    </div>
+    <div class="setting-modal-main" v-if="step == 1">
+        <p>Are you sure deactive your account?</p>
+        <div class="setting-buttons">
+            <div class="modal-btn" @click="nextModal()">Yes</div>
+            <div class="modal-btn">No</div>
+        </div>
+    </div>
+     <div class="add-password-sec" v-if="step == 2">
+        <p>Add password to confirm</p>
+        <div class="pass-input">
+            <img src="../../assets/image/lock.svg" alt="lock icon" class="lock" />
+            <input
+              :type="yourPasswordVisible ? 'text' : 'password'"
+              class="password"
+              placeholder="Your password"
+            />
+            <div @click="yourPasswordVisible = !yourPasswordVisible">
+              <i
+                class="eye-icon"
+                :class="
+                yourPasswordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'
+                "
+              ></i>
+            </div>
+          </div>
+          <button class="deactive-btn" @click="nextModal()">Deactive</button>
+     </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "settingModal",
+  data() {
+    return {
+        step: 1,
+        yourPasswordVisible: false,
+    }
+  },
+  methods: {
+    nextModal() {
+      this.step++;
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
