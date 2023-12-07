fc1f871b3e245450186d625bdd3050e455f5cf53
lalaibrahimzade <lalaibrahimzade19@gmail.com>
2023-12-07 15:33:19 +0300

    setting navbar saerch icon style changed

--- layouts/footer-section.vue
+++ layouts/footer-section.vue
@@ -1,13 +1,13 @@
 <template>
-
+  <div>
+    <h1>FOOTER</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "footer"
-}
+  name: "footer",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
